27bfb748a6b7c18a76db1454ee5e91541e82793b
Allie <allie@iqengines.com>
2012-06-23 18:12:48 -0700

    Initial attempt at site

---
+++ Sarahs_website/Production_Photos/Said_and_Meant_files/Said_and_Meant.js
@@ -0,0 +1,29 @@
+// Created by iWeb 3.0.3 local-build-20120623
+
+function createMediaStream_id2()
+{return IWCreatePhotocast("file://localhost/Users/allie/Misc/SarahWebsite/alliejanoch.github.com/Sarahs_website/Production_Photos/Said_and_Meant_files/rss.xml",false);}
+function initializeMediaStream_id2()
+{createMediaStream_id2().load('file://localhost/Users/allie/Misc/SarahWebsite/alliejanoch.github.com/Sarahs_website/Production_Photos',function(imageStream)
+{var entryCount=imageStream.length;var headerView=widgets['widget1'];headerView.setPreferenceForKey(imageStream.length,'entryCount');NotificationCenter.postNotification(new IWNotification('SetPage','id2',{pageIndex:0}));});}
+function layoutMediaGrid_id2(range)
+{createMediaStream_id2().load('file://localhost/Users/allie/Misc/SarahWebsite/alliejanoch.github.com/Sarahs_website/Production_Photos',function(imageStream)
+{if(range==null)
+{range=new IWRange(0,imageStream.length);}
+IWLayoutPhotoGrid('id2',new IWPhotoGridLayout(3,new IWSize(166,166),new IWSize(166,32),new IWSize(204,213),27,27,0,new IWSize(0,0)),new IWEmptyStroke(),imageStream,range,null,null,1.000000,null,'Media/slideshow.html','widget1','widget2','widget3')});}
+function relayoutMediaGrid_id2(notification)
+{var userInfo=notification.userInfo();var range=userInfo['range'];layoutMediaGrid_id2(range);}
+function onStubPage()
+{var args=window.location.href.toQueryParams();parent.IWMediaStreamPhotoPageSetMediaStream(createMediaStream_id2(),args.id);}
+if(window.stubPage)
+{onStubPage();}
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{IWRegisterNamedImage('comment overlay','Media/Photo-Overlay-Comment.png')
+IWRegisterNamedImage('movie overlay','Media/Photo-Overlay-Movie.png')
+loadMozillaCSS('Said_and_Meant_files/Said_and_MeantMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');NotificationCenter.addObserver(null,relayoutMediaGrid_id2,'RangeChanged','id2')
+Widget.onload();fixAllIEPNGs('Media/transparent.gif');initializeMediaStream_id2()
+performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
