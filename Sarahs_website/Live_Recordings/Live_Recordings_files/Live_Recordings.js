15379bd54d4eba777e3e6f6d013a3a09b4b25b7c
Allie <allie@iqengines.com>
2012-07-25 15:21:16 -0700

    Oops, forgot live recordings page

---
+++ Sarahs_website/Live_Recordings/Live_Recordings_files/Live_Recordings.js
@@ -0,0 +1,13 @@
+// Created by iWeb 3.0.3 local-build-20120723
+
+setTransparentGifURL('../Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({reflection_0:new IWReflection({opacity:0.50,offset:1.00})});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function photocastSubscribe()
+{photocastHelper("file://localhost/Users/allie/Misc/SarahWebsite/alliejanoch.github.com/Sarahs_website/Live_Recordings/rss.xml");}
+function onPageLoad()
+{loadMozillaCSS('Live_Recordings_files/Live_RecordingsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixAllIEPNGs('../Media/transparent.gif');fixupAllIEPNGBGs();applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
