48527c59ab8bf2ad22c45aafefbb8dd2587867bf
Allie Janoch <allie@iqengines.com>
2013-06-13 11:30:21 -0700

    Add new recording of aint no sunshine. Also updated home page.

--- Sarahs_website/Home_files/Home.js
+++ Sarahs_website/Home_files/Home.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.4 local-build-20130115
+// Created by iWeb 3.0.4 local-build-20130613
 
 setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
 {return false;}
---
+++ Sarahs_website/Live_Recordings/Entries/2013/6/13_Aint_No_Sunshine_files/13_Aint_No_Sunshine.js
@@ -0,0 +1,16 @@
+// Created by iWeb 3.0.4 local-build-20130613
+
+function writeMovie1()
+{detectBrowser();if(windowsInternetExplorer)
+{document.write('<object id="id3" classid="clsid:02BF25D5-8C17-4B23-BC80-D3488ABDDC6B" codebase="http://www.apple.com/qtactivex/qtplugin.cab" width="300" height="241" style="height: 241px; left: 200px; position: absolute; top: 58px; width: 300px; z-index: 1; "><param name="src" value="../../../../Media/2013-04-21%2007.04.12.mov" /><param name="controller" value="true" /><param name="autoplay" value="false" /><param name="scale" value="tofit" /><param name="volume" value="100" /><param name="loop" value="false" /></object>');}
+else if(isiPhone)
+{document.write('<object id="id3" type="video/quicktime" width="300" height="241" style="height: 241px; left: 200px; position: absolute; top: 58px; width: 300px; z-index: 1; "><param name="src" value="13_Aint_No_Sunshine_files/2013-04-21%2007.04.12.jpg"/><param name="target" value="myself"/><param name="href" value="../../../../../Media/2013-04-21%2007.04.12.mov"/><param name="controller" value="true"/><param name="scale" value="tofit"/></object>');}
+else
+{document.write('<object id="id3" type="video/quicktime" width="300" height="241" data="../../../../Media/2013-04-21%2007.04.12.mov" style="height: 241px; left: 200px; position: absolute; top: 58px; width: 300px; z-index: 1; "><param name="src" value="../../../../Media/2013-04-21%2007.04.12.mov"/><param name="controller" value="true"/><param name="autoplay" value="false"/><param name="scale" value="tofit"/><param name="volume" value="100"/><param name="loop" value="false"/></object>');}}
+setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{dynamicallyPopulate();loadMozillaCSS('13_Aint_No_Sunshine_files/13_Aint_No_SunshineMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');adjustLineHeightIfTooBig('id7');adjustFontSizeIfTooBig('id7');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id5');BlogFixupPreviousNext();performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
